7f7c45523c2d8b34dc440334a92c0d212f7a5aae
mahteuso <mateus@ufscar.br>
2024-09-20 09:57:36 -0300

    Config: Atualizando projeto

---
+++ src/main/java/com/mateus/park_api/jwt/JwtToken.java
@@ -0,0 +1,16 @@
+package com.mateus.park_api.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+
+    private String token;
+
+}
